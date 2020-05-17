03b67971416808e6933b551b5d35f1b0a4878039
TinaTabo <60134309+TinaTabo@users.noreply.github.com>
2020-05-17 11:58:33 +0200

    P4 casi casi terminada

--- P4/video.js
+++ P4/video.js
@@ -36,37 +36,62 @@ imagen.height = 150;
 // Control de funcionamiento de los botones.
 
 // PLAYER1 - video1 - BTS-BlackSwan
+// Quita la imagen fija o el video que se estaba reproduciendo anteriormente
+// en el mainplayer y empieza a reproducir el video 1 en el mainplayer.
+const play1 = document.getElementById("play1");
 play1.onclick = () =>{
   console.log("Reproduciendo BTS-BlackSwan");
   video0.poster = false;
   video0.src= video1.src;
   video0.muted = false;
   video0.currentTime = video1.currentTime;
-
+  video1.style.border = 'groove #6F6F76';
+  video2.style.border = null;
+  video3.style.border = null;
+  imagen.style.border = null;
 };
 
 // PLAYER2 - video2 - BTS-NotToday
+// Quita la imagen fija o el video que se estaba reproduciendo anteriormente
+// en el mainplayer y empieza a reproducir el video 2 en el mainplayer.
+const play2 = document.getElementById("play2");
 play2.onclick = () =>{
   console.log("Reproduciendo BTS-NotToday");
   video0.poster = false;
   video0.src= video2.src;
   video0.muted = false;
   video0.currentTime = video2.currentTime;
-
+  video1.style.border = null;
+  video2.style.border = 'groove #6F6F76';
+  video3.style.border = null;
+  imagen.style.border = null;
 };
 
 // PLAYER3 - video3 - BTS-Idol
+// Quita la imagen fija o el video que se estaba reproduciendo anteriormente
+// en el mainplayer y empieza a reproducir el video 3 en el mainplayer.
+const play3 = document.getElementById("play3");
 play3.onclick = () =>{
   console.log("Reproduciendo BTS-Idol");
   video0.poster = false;
   video0.src= video3.src;
   video0.muted = false;
   video0.currentTime = video3.currentTime;
-
+  video1.style.border = null;
+  video2.style.border = null;
+  video3.style.border = 'groove #6F6F76';
+  imagen.style.border = null;
 };
 
 // PLAYER4
+// Elimina la fuente de video del reproductor principal y muestra la imagen fija
+// de las barras de colores.
+const play4 = document.getElementById("play4");
 play4.onclick = () =>{
   video0.poster = imagen.src;
   video0.src = null;
+  video1.style.border = null;
+  video2.style.border = null;
+  video3.style.border = null;
+  imagen.style.border = 'groove #6F6F76';
 }
