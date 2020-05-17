6d2441866c002b6924d22120ae810305515d7a8d
TinaTabo <60134309+TinaTabo@users.noreply.github.com>
2020-05-17 13:59:45 +0200

    paso 3

--- P5/filtro.js
+++ P5/filtro.js
@@ -24,6 +24,25 @@ img.onload = function () {
 
   // Obtener el array con todos los pixeles
   let data = imgData.data;
+  //-- data[0] es el canal rojo del pixel de la posición 0,0
+  //-- data[1] es el canal verde del pixel 0,0
+  //-- data[2] es el canal azul del pixel 0,0
+  //-- data[3] es el canal de transparencia del pixel 0,0
+  //-- data[4] es el canal ROJO del pixel 1,0
+  //-- ....
+  //-- En general, para el pixel i
+  //-- data[4*i] es el canal rojo
+  //-- data[4*i + 1]: Canal verde
+  //-- data[4*i + 2]: Canal azul
+  //-- data[4*i + 3]: Canal de transparencia
+  //-- Colocar un pixel rojo en (200,50)
+  //-- Obtener el numero de pixel a partir de su posicion
+  let i = 200 + 50*canvas.width;
+  //-- Pixel rojo: canal rojo a tope. Resto de colores a 0
+  //-- La transparencia no se modifica
+  data[i*4] = 255; //-- Canal Rojo
+  data[i*4 + 1] = 0; //-- Canal Verde
+  data[i*4 + 2] = 0; //-- Canal azul
 
   // Obtener el numero total de elementos del array
   console.log("Tamaño de data: " + data.length);
